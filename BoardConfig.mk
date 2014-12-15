CyanogenMod/android_device_motorola_ghost

ghost: Add device-specific SELinux policies
Change-Id: Ic368cecd7e95c61c42ed290b5f47d3e845d50359

BoardConfig.mk
@@ -134,6 +134,16 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    keystore.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    system_server.te \
+    ueventd.te
+
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 

AICP/device_motorola_ghost

BoardConfig.mk
@@ -132,6 +132,22 @@ TARGET_USERIMAGES_USE_F2FS := true
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += \
+    device/motorola/ghost/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    device.te \
+    file_contexts \
+    file.te \
+    init.te \
+    keystore.te \
+    mm-pp-daemon.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    platform_app.te \
+    thermal-engine.te \
+    ueventd.te
+
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 